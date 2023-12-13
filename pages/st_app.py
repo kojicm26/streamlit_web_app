c594193240b14eb4c180081a9cc884d7858ae95e
kojicm26 <ko.ji.1101050@gmail.com>
2023-12-13 11:17:26 +0900

    20231213-02

---
+++ pages/st_app.py
@@ -0,0 +1,24 @@
+# example/st_app.py
+
+import streamlit as st
+# from streamlit_gsheets import GSheetsConnection
+from PIL import Image
+
+url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
+
+# Please replace st.experimental_connection with st.connection.
+# st.experimental_connection を st.connection に置き換えてください。
+# conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+# conn = st.connection("gsheets", type=GSheetsConnection)
+
+
+# data = conn.read(spreadsheet=url, usecols=[0, 1])
+# st.dataframe(data)
+
+
+#テキスト関連
+st.title('サプーアプリ')
+st.caption('これはサプーの動画用のテストアプリです')
+#画像
+image = Image.open('./data/supu.jpg')
+st.image(image, width=500)
