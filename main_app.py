3ad20700f3151a3e02be4971fb5f289e8c2be97c
kojicm26 <ko.ji.1101050@gmail.com>
2023-12-18 14:26:29 +0900

    20231218-07

--- main_app.py
+++ main_app.py
@@ -12,7 +12,8 @@ st.image(image, width=100)
 
 url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing" #サンプル(date&births)
 
-conn = st.connection("gsheets", type=GSheetsConnection)
+conn = st.experimental_connection("gsheets", type=GSheetsConnection)
+# conn = st.connection("gsheets", type=GSheetsConnection)
 
 df = conn.read(spreadsheet=url, usecols=["date"])
 st.title("date&births")
