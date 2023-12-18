88c65cebdcd714d066e49290037b3632473e1adb
kojicm26 <ko.ji.1101050@gmail.com>
2023-12-18 11:03:34 +0900

    20231218-02

--- main_app.py
+++ main_app.py
@@ -1,3 +1,5 @@
 import streamlit as st
 from PIL import Image
 from streamlit_gsheets import GSheetsConnection
+
+url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
