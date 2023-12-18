8f653ac0ae03e60485980cde66ab868eefbc1afc
kojicm26 <ko.ji.1101050@gmail.com>
2023-12-18 11:16:42 +0900

    20231218-04

--- main_app.py
+++ main_app.py
@@ -2,6 +2,14 @@ import streamlit as st
 from PIL import Image
 from streamlit_gsheets import GSheetsConnection
 
+#テキスト関連
+st.title('サプーアプリ')
+st.caption('これはサプーの動画用のテストアプリです')
+#画像
+image = Image.open('./data/supu.jpg')
+st.image(image, width=100)
+
+
 url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
 
 conn = st.connection("gsheets", type=GSheetsConnection)
