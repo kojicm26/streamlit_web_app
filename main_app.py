ff43f3687500a3304b0e0a7db0fd2b12604ff3c7
kojicm26 <ko.ji.1101050@gmail.com>
2023-12-18 14:17:16 +0900

    20231218-06

--- main_app.py
+++ main_app.py
@@ -10,6 +10,10 @@ image = Image.open('./data/supu.jpg')
 st.image(image, width=100)
 
 
-url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
+url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing" #サンプル(date&births)
 
 conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read(spreadsheet=url, usecols=["date"])
+st.title("date&births")
+st.write(df)
--- pages/page_2.py
+++ pages/page_2.py
@@ -1,19 +1,30 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+
+#ラジオボタン
+source_d_type = st.radio(
+        '情報データ元の種類',
+        ('Gドライブのurl', 'csv直接入力')
+)
 
 with st.form(key='profile_form'):
-    #テキストボックス
-    name = st.text_input('名前')
-    address = st.text_input('住所')
+        #テキストボックス
+        # name = st.text_input('名前')
 
-    #ラジオボタン
-    age_category = st.radio(
-            '年齢層',
-            ('子ども(18歳未満)', '大人(18歳以上)')
-    )
+        url = st.text_input('Googleスプレッドシート（Gドライブのurl）')
+        conn = st.connection("gsheets", type=GSheetsConnection)
 
-    #ボタン
-    submit_btn = st.form_submit_button('送信')
-    cancel_btn = st.form_submit_button('キャンセル')
-    if submit_btn:
-            st.text(f'ようこそ{name}さん!{address}に書籍を送りました！')
-            st.text(f'年齢層： {age_category}')
+        # address = st.text_input('住所')
+        csv = st.text_area('CSVデータなど')
+
+
+        #ボタン
+        submit_btn = st.form_submit_button('送信')
+        cancel_btn = st.form_submit_button('キャンセル')
+        if submit_btn:
+                st.text(f'情報データ元の種類： {source_d_type}')
+                # st.text(f'ようこそ{name}さん!{address}に書籍を送りました！')
+                df = conn.read(spreadsheet=url)
+                st.title("date&births")
+                st.write(df)
+                # st.text(f'年齢層： {age_category}')
